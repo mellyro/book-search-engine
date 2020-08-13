abb721105176bea5c11997b1498c2d73a57c7d12
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-08-13 13:37:27 -0500

    add queries and mutations

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import {useMutation} from '@apollo/react-hooks';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import {  searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import {SAVE_BOOK} from '../utils/mutations'
+
 
 const SearchBooks = () => {
+  const [saveBook, {error}] = useMutation(SAVE_BOOK)
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -65,12 +68,13 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
+    const {data} =  await saveBook({
+       variables:{input: bookToSave}
+      
+      });
+      if (error) {
         throw new Error('something went wrong!');
       }
-
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
